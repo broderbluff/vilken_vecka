e3142e6fb0589e33c02ff9a87a48ae486e17f649
Patrik <broderbluff@gmail.com>
2022-03-14 20:24:29 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/vilken_vecka/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vilken_vecka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:isoweek/isoweek.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vilken vecka',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Week currentWeek = Week.current();
+
+  List<Widget> daysWidgets(List<DateTime> days) {
+    final dateFormat = DateFormat('EEE');
+    String locale = Localizations.localeOf(context).languageCode;
+
+    List<Widget> widgets = [];
+    days.forEach((element) {
+      String readableDay = dateFormat.format(element);
+      widgets.add(Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: [
+          Text(
+            '${element.day}/${element.month}',
+            textScaleFactor: 1.5,
+            style: GoogleFonts.firaSans(
+                textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: readableDay == 'Sun' ? Colors.red : Colors.black)),
+          ),
+          Text(
+            readableDay,
+            style: TextStyle(
+                color: readableDay == 'Sun' ? Colors.red : Colors.black),
+          )
+        ]),
+      ));
+    });
+
+    return widgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String result = currentWeek.weekNumber.toString();
+    print(currentWeek.days);
+
+    List<DateTime> days = currentWeek.days;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Det är nu vecka:',
+              textScaleFactor: 2,
+              style: GoogleFonts.firaSansCondensed(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: SizedBox(
+                height: 500,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    result,
+                    style: GoogleFonts.anton(),
+                    textScaleFactor: 100,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [...daysWidgets(days)],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
